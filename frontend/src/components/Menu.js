4b1dfbeb3e9a39e31eeec54f19ddea7daf8b1374
fhrmt <feher.mate.944@gmail.com>
2021-02-09 15:16:32 +0100

    each menu component navigates to their own path

--- frontend/src/components/Menu.js
+++ frontend/src/components/Menu.js
@@ -1,12 +1,51 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
-const items = ["Felhasználók", "Termékek", "Események", "Kijelentkezés"];
+const items = ["menu.users", "menu.products", "menu.events", "menu.logout"];
 
 export default function Menu() {
     return (
-
-
-        
-        );
-    }
+      <div className="d-flex" id="wrapper">
+        <div className="bg-light border-right" id="sidebar-wrapper">
+          <div className="list-group list-group-flush">
+            <div key={items[0]} className="nav-item">
+              <NavLink
+                activeClassName="active"
+                className="nav-link"
+                to={`/users`}
+              >
+                {items[0]}
+              </NavLink>
+            </div>
+            <div key={items[1]} className="nav-item">
+              <NavLink
+                activeClassName="active"
+                className="nav-link"
+                to={`/products`}
+              >
+                {items[1]}
+              </NavLink>
+            </div>
+            <div key={items[2]} className="nav-item">
+              <NavLink
+                activeClassName="active"
+                className="nav-link"
+                to={`/events`}
+              >
+                {items[2]}
+              </NavLink>
+            </div>
+            <div key={items[3]} className="nav-item">
+              <NavLink
+                activeClassName="active"
+                className="nav-link"
+                to={`/logout`}
+              >
+                {items[3]}
+              </NavLink>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
