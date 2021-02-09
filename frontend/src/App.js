2c8ad9ed3b86279b2bfc98594692ac901e161fcb
Dominik <gosidominik@gmail.com>
2021-02-09 09:09:14 +0100

    Rrouter moved from index.js to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,24 @@
 import "./App.css";
-import { Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <Switch>
-      <Route path="/registration">
-        <div>Registration component</div>
-      </Route>
-      <Route path="/companyAdmin">
-        <div>Company</div>
-      </Route>
-      <Route path="/superAdmin">
-        <div>Superadmin</div>
-      </Route>
-      <Route path="/login">
-        <div>Login</div>
-      </Route>
-    </Switch>
+    <Router>
+      <Switch>
+        <Route path="/registration">
+          <div>Registration component</div>
+        </Route>
+        <Route path="/companyAdmin">
+          <div>Company</div>
+        </Route>
+        <Route path="/superAdmin">
+          <div>Superadmin</div>
+        </Route>
+        <Route path="/login">
+          <div>Login</div>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
