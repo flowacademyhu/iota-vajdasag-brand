e8aeb2bdc975a86f3c9febbfe706e9d9b9a275d4
fhrmt <feher.mate.944@gmail.com>
2021-02-09 11:07:10 +0100

    menu sidebar items naming convention

---
+++ frontend/src/components/Menu.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const items = ["Felhasználók", "Termékek", "Események", "Kijelentkezés"];
+
+export default function Menu() {
+    return (
+
+
+        
+        );
+    }
