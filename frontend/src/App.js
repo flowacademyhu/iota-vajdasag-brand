b989afd92b686f681a34b9f99732e090d6c447ff
Tibi <tothtibi30@gmail.com>
2021-02-10 14:36:23 +0100

    Added scanner to basic i18n

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,17 @@
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import SwitchLanguage from "./components/SwitchLanguage";
+import { useTranslation } from "react-i18next";
 
 function App() {
+  const { t } = useTranslation();
+
   return (
     <Router>
       <Switch>
         <Route path="/registration">
-          <div>Registration component</div>
+          <div>{t("registration")}</div>
+          <SwitchLanguage />
         </Route>
         <Route path="/company-admin">
           <div>Company</div>
--- frontend/src/components/SwitchLanguage.js
+++ frontend/src/components/SwitchLanguage.js
@@ -2,20 +2,26 @@ import React from "react";
 import { useTranslation } from "react-i18next";
 
 const SwitchLanguage = () => {
-  const { i18n } = useTranslation();
+  const { t, i18n } = useTranslation();
+
+  const actualLanguage = i18n.language;
+
+  const handleChange = (e) => {
+    i18n.changeLanguage(e.target.value);
+  };
 
   return (
     <div className="col-2">
       <select
         className="form-select mb-3"
-        onClick={(e) => i18n.changeLanguage(e.target.value)}
+        value={actualLanguage}
+        onChange={handleChange}
       >
-        <option value="hu" selected>
-          🇭🇺 Magyar
-        </option>
+        <option value="hu">🇭🇺 Magyar</option>
         <option value="sr">🇷🇸 Српски</option>
         <option value="en">🇬🇧 English</option>
       </select>
+      <p>{t("text")}</p>
     </div>
   );
 };
