73f2a70b92c8ee1830936064615f24c4a170517c
Hegedűs Barbara <hegedusbarby@gmail.com>
2021-02-10 15:59:50 +0100

    application.yaml config

--- backend/src/main/java/hu/flowacademy/vajdasagbrand/userRepository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package hu.flowacademy.vajdasagbrand.userRepository;
-
-import hu.flowacademy.vajdasagbrand.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, String> {
-}
