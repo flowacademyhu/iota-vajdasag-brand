15abc896871eae00af998f3cd80a12229dc55a81
Tibi <tothtibi30@gmail.com>
2021-02-10 14:36:23 +0100

    Added scanner to basic i18n

--- frontend/src/i18n.js
+++ frontend/src/i18n.js
@@ -1,14 +1,18 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import TranslationsFromJson from "./translations.json";
+import en from "./i18n/en.json";
+import hu from "./i18n/hu.json";
+import sr from "./i18n/sr.json";
 
-const resources = TranslationsFromJson;
+const resources = { en, hu, sr };
 
 i18n.use(initReactI18next).init({
   resources,
-  lng: "hu",
+  lng: "en",
 
-  keySeparator: false,
+  keySeparator: ".",
+  defaultNS: "",
+  ns: "",
 
   interpolation: {
     escapeValue: false,
