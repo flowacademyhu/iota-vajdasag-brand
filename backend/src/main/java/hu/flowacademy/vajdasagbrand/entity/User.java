213b5267f159b8c3b62e9cc603b48f28d6ea20bc
Hegedűs Barbara <hegedusbarby@gmail.com>
2021-02-10 16:04:37 +0100

    created User class

--- backend/src/main/java/hu/flowacademy/vajdasagbrand/entity/User.java
+++ backend/src/main/java/hu/flowacademy/vajdasagbrand/entity/User.java
@@ -1,12 +1,17 @@
 package hu.flowacademy.vajdasagbrand.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import hu.flowacademy.vajdasagbrand.exception.ValidationException;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.time.LocalDateTime;
 
 @Entity
+@NoArgsConstructor
 public class User {
 
     @Id
@@ -17,6 +22,60 @@ public class User {
     private String taxNumber;
     private String address;
     private String email;
+    private String password;
+    private Type type;
+    private boolean enabled;
+    @JsonFormat(pattern = ("yyyy/MM/dd HH:mm:ss"))
+    private LocalDateTime localDateTime;
+
+    public User(String fullName, String taxNumber, String address, String email, String password, String type) throws ValidationException {
+        this.fullName = fullName;
+        this.taxNumber = taxNumber;
+        this.address = address;
+        this.email = email;
+        this.password = password;
+        if(type.equals("INDIVIDUAL")) {
+            this.type = Type.INDIVIDUAL;
+        } else if(type.equals("COMPANY")) {
+            this.type = Type.COMPANY;
+        } else {
+            throw new ValidationException("Bad input");
+        }
+        this.enabled = true;
+        this.localDateTime = LocalDateTime.now();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDateTime getLocalDateTime() {
+        return localDateTime;
+    }
+
+    public void setLocalDateTime(LocalDateTime localDateTime) {
+        this.localDateTime = localDateTime;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
 
     public String getId() {
         return id;
