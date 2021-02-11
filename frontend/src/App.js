73b268425dfd06901eb16588024458cb7931ab62
fhrmt <feher.mate.944@gmail.com>
2021-02-11 15:06:09 +0100

    menu sidebar only shows when we are logged in thanks to redirection

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,44 @@
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 import SwitchLanguage from "./components/SwitchLanguage";
 import { useTranslation } from "react-i18next";
-import Menu from "./Menu.js";
-import { useEffect, useState } from "react";
+import Menu from "./components/Menu.js";
 
-function App() {
+export default function App() {
   const { t } = useTranslation();
-  const [isLoggedIn, setIsloggedIn] = useState(false);
-  
+  const isLoggedIn = false;
+  const loggedInAsSuperAdmin = false;
+  const loggedInAsCompanyAdmin = false;
 
   return (
     <Router>
-      <Switch>
-        <Route path="/registration">
-          <div>{t("registration")}</div>
-          <SwitchLanguage />
-        </Route>
-        <Route path="/login">
-          <div>Login</div>
-        </Route>
-        <Route path="/company-admin">
-          <div id="page-content-wrapper">Company</div>
-        </Route>
-        <Route path="/super-admin">
-          <div>Superadmin</div>
-        </Route>      
-      </Switch>
+      <div>
+        {isLoggedIn ? <Menu /> : <Redirect to="/registration" />}
+        {loggedInAsCompanyAdmin && <Redirect to="/company-admin" />}
+        {loggedInAsSuperAdmin && <Redirect to="/super-admin" />}
+      </div>
+      <div>
+        <Switch>
+          <Route path="/registration">
+            <SwitchLanguage />
+            <div>{t("registration")}</div>
+          </Route>
+          <Route path="/login">
+            <div>Login</div>
+          </Route>
+          <Route path="/company-admin">
+            <div id="page-content-wrapper">Company</div>
+          </Route>
+          <Route path="/super-admin">
+            <div>Superadmin</div>
+          </Route>
+        </Switch>
+      </div>
     </Router>
   );
 }
-
-export default App;
