7facca3b9a1963ea9ccb320d06af2b51c523ee7e
Hegedűs Barbara <hegedusbarby@gmail.com>
2021-02-11 15:43:44 +0100

    userReg gets validations

--- backend/src/main/java/hu/flowacademy/vajdasagbrand/service/UserService.java
+++ backend/src/main/java/hu/flowacademy/vajdasagbrand/service/UserService.java
@@ -1,33 +1,46 @@
 package hu.flowacademy.vajdasagbrand.service;
 
 import hu.flowacademy.vajdasagbrand.entity.Type;
+import org.apache.commons.validator.routines.EmailValidator;
 import hu.flowacademy.vajdasagbrand.entity.User;
 import hu.flowacademy.vajdasagbrand.exception.ValidationException;
 import hu.flowacademy.vajdasagbrand.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
+
+import java.time.LocalDateTime;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class UserService {
 
-
     private final UserRepository userRepository;
 
-
     public User userRegistrationData(User user) throws ValidationException {
-        if (StringUtils.hasText(user.getFullName())) {
+        if (!StringUtils.hasText(user.getFullName())) {
             throw new ValidationException("Didn't get full name", HttpStatus.BAD_REQUEST);
         }
+        if(!StringUtils.hasText(user.getAddress())){
+            throw new ValidationException("Didn't get address", HttpStatus.BAD_REQUEST);
+        }
+        if(!StringUtils.hasText(user.getEmail())){
+            throw new ValidationException("Didn't get email", HttpStatus.BAD_REQUEST);
+        }
+        if(!EmailValidator.getInstance().isValid(user.getEmail())){
+            throw new ValidationException("Invalid email", HttpStatus.BAD_REQUEST);
+        }
+        if(!StringUtils.hasText(String.valueOf(user.getType()))) {
+            throw new ValidationException("Didn't get type", HttpStatus.BAD_REQUEST);
+        }
+        if((user.getTaxNumber()).isEmpty() && user.getType() == Type.COMPANY){
+            throw new ValidationException("Can not add tax number for individual members", HttpStatus.BAD_REQUEST);
+        }
+        user.setLocalDateTime(LocalDateTime.now());
         User result = userRepository.save(user);
-        log.debug("The result is : {}",result);
         return result;
     }
 }
