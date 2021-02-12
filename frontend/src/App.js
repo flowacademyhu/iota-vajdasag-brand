5cf0060327f9d20bc0b06c0b9d6063cf3ede6867
pakaikarolina <pakaikarolina@gmail.com>
2021-02-12 11:01:42 +0100

    moved registration.js to pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import Registration from "./components/Registration";
+import Registration from "./pages/Registration";
 import Login from "./pages/Login";
 import {
   BrowserRouter as Router,
