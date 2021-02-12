6f420b841d570063a1ccec5b5a1e5cc19b457ebb
Dominik <gosidominik@gmail.com>
2021-02-12 15:14:26 +0100

    skeleton for delete endpoint

--- backend/src/main/java/hu/flowacademy/vajdasagbrand/controller/UserController.java
+++ backend/src/main/java/hu/flowacademy/vajdasagbrand/controller/UserController.java
@@ -1,12 +1,19 @@
 package hu.flowacademy.vajdasagbrand.controller;
 
 import hu.flowacademy.vajdasagbrand.dto.LoginDto;
+import hu.flowacademy.vajdasagbrand.entity.User;
+import hu.flowacademy.vajdasagbrand.exception.ValidationException;
 import hu.flowacademy.vajdasagbrand.service.KeycloakClientService;
+import hu.flowacademy.vajdasagbrand.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.keycloak.representations.AccessTokenResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import javax.annotation.security.PermitAll;
+import javax.annotation.security.RolesAllowed;
+
 
 @Slf4j
 @RequiredArgsConstructor
@@ -15,10 +22,22 @@ import javax.annotation.security.PermitAll;
 public class UserController {
 
     private final KeycloakClientService keycloakClientService;
+    private final UserService userService;
 
     @PermitAll
     @PostMapping("/login")
     public AccessTokenResponse login(@RequestBody LoginDto loginDto) {
         return keycloakClientService.login(loginDto.getUsername(),loginDto.getPassword());
+    }
+
+    @RolesAllowed("realmadmin")
+    @DeleteMapping("/delete-user/{id}")
+    public User deleteUser(@PathVariable("id") String id) {
+        try {
+            User user = userService.deleteById(id);
+            return user;
+        } catch (ValidationException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found");
+        }
     }
 }
