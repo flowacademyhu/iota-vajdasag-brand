f29de37c25970a95f68e50e0db535dca305ebcdc
Tibi <tothtibi30@gmail.com>
2021-02-14 18:42:17 +0100

    Organized filtering into useUsers hook

--- frontend/src/components/useUsers.js
+++ frontend/src/components/useUsers.js
@@ -1,17 +1,35 @@
 import { useState, useEffect } from "react";
 import { getUsers } from "../communications/userApi";
 
-const useUsers = () => {
+const useUsers = (searchKeyword) => {
+  const [listOfAllUsers, setListOfAllUsers] = useState([]);
   const [users, setUsers] = useState([]);
 
   const fetchUsers = async () => {
     const fetchedUsers = await getUsers();
-    setUsers(fetchedUsers);
+    setListOfAllUsers(fetchedUsers);
   };
 
   useEffect(() => {
     fetchUsers();
   }, []);
+
+  const makeWordComparable = (keyword) => {
+    return keyword
+      .normalize("NFD")
+      .replace(/[\u0300-\u036f]/g, "")
+      .toUpperCase();
+  };
+
+  useEffect(() => {
+    setUsers(
+      listOfAllUsers?.filter((user) =>
+        makeWordComparable(user.name).includes(
+          makeWordComparable(searchKeyword)
+        )
+      )
+    );
+  }, [listOfAllUsers, searchKeyword]);
 
   return { users };
 };
