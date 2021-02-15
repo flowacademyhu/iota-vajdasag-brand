52cae4915a081ae42ffd186350ec07f140d42fce
Tibi <tothtibi30@gmail.com>
2021-02-15 13:26:14 +0100

    Added states to store variables for sorting, implemented sort method on users array

--- frontend/src/components/UsersList.js
+++ frontend/src/components/UsersList.js
@@ -1,31 +1,39 @@
 import React, { useState } from "react";
-import ListElement from "./listofusers/ListElement";
-import { useTranslation } from "react-i18next";
 import useUsers from "./useUsers";
+import ListElement from "./listofusers/ListElement";
 import Searchbar from "./listofusers/Searchbar";
+import ListHeader from "./listofusers/ListHeader";
 
 const UsersList = () => {
   const [searchKeyword, setSearchKeyword] = useState("");
+  const [sortKey, setSortKey] = useState("");
+  const [isSortAscending, setAscendingSort] = useState(true);
   const { users } = useUsers(searchKeyword);
-  const { t } = useTranslation();
+
+  const sortColumn = (a, b) => {
+    if (isSortAscending) {
+      return a[sortKey] > b[sortKey] ? 1 : -1;
+    } else {
+      return a[sortKey] < b[sortKey] ? 1 : -1;
+    }
+  };
 
   return (
     <div className="d-flex flex-row-reverse">
       <div className="col-9">
         <Searchbar setSearchKeyword={setSearchKeyword} />
         <table className="table table-striped">
-          <thead>
-            <tr>
-              <th scope="col">{t("name")}</th>
-              <th scope="col">{t("email")}</th>
-              <th scope="col">{t("acceptedRegistration")}</th>
-              <th scope="col">{t("dateOfRegistration")}</th>
-            </tr>
-          </thead>
+          <ListHeader
+            setSortKey={setSortKey}
+            setAscendingSort={setAscendingSort}
+            isSortAscending={isSortAscending}
+          />
           <tbody>
-            {users?.map((user) => (
-              <ListElement user={user} key={user.id} />
-            ))}
+            {users
+              ?.sort((a, b) => sortColumn(a, b))
+              .map((user) => (
+                <ListElement user={user} key={user.id} />
+              ))}
           </tbody>
         </table>
       </div>
