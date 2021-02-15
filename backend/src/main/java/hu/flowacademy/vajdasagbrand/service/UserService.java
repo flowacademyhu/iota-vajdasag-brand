37ae3bfd54c4fd0a836d8e6acf5e5dae4ebbdcb0
Dominik <gosidominik@gmail.com>
2021-02-15 15:15:20 +0100

    delete sets user deletedAt status to actual time

--- backend/src/main/java/hu/flowacademy/vajdasagbrand/service/UserService.java
+++ backend/src/main/java/hu/flowacademy/vajdasagbrand/service/UserService.java
@@ -4,12 +4,15 @@ import hu.flowacademy.vajdasagbrand.entity.User;
 import hu.flowacademy.vajdasagbrand.exception.ValidationException;
 import hu.flowacademy.vajdasagbrand.repository.UserRepository;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.keycloak.admin.client.Keycloak;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
 
+@Slf4j
 @Service
 @AllArgsConstructor
 public class UserService {
@@ -17,18 +20,17 @@ public class UserService {
     private final UserRepository userRepository;
     private final KeycloakClientService keycloakClientService;
 
-    public User deleteById(String id) throws ValidationException {
+    public void deleteById(String id) throws ValidationException {
        Optional<User> user = userRepository.findById(id);
        if(user.isEmpty()) {
            throw new ValidationException("No user with given id: " + id);
        }
-       if(!user.get().getDeletedAt().equals(null)) {
+       if(user.get().getDeletedAt() != null) {
            throw new ValidationException("User already deleted");
        }
        User deleted = user.get();
        keycloakClientService.deleteUser(deleted.getEmail());
        deleted.setDeletedAt(LocalDateTime.now());
        userRepository.save(deleted);
-       return deleted;
     }
 }
