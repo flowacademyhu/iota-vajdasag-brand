e63ab9aca92cc7f6c0faf3cbf11d66633f56a113
Dominik <gosidominik@gmail.com>
2021-02-15 10:29:12 +0100

    delete updated in keycloakservice

--- backend/src/main/java/hu/flowacademy/vajdasagbrand/service/KeycloakClientService.java
+++ backend/src/main/java/hu/flowacademy/vajdasagbrand/service/KeycloakClientService.java
@@ -1,20 +1,9 @@
 package hu.flowacademy.vajdasagbrand.service;
 
-import org.keycloak.admin.client.CreatedResponseUtil;
-import org.keycloak.admin.client.resource.*;
 import org.keycloak.representations.AccessTokenResponse;
-import org.keycloak.representations.idm.CredentialRepresentation;
-import org.keycloak.representations.idm.RoleRepresentation;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.keycloak.admin.client.Keycloak;
-import org.keycloak.admin.client.KeycloakBuilder;
-import org.keycloak.representations.idm.UserRepresentation;
-import javax.ws.rs.core.Response;
-import java.net.URI;
-import java.util.Arrays;
-import java.util.Locale;
 
 @Service
 public class KeycloakClientService {
@@ -32,70 +21,6 @@ public class KeycloakClientService {
     @Value("${keycloak.credentials.secret}")
     private String clientsecret;
 
-    private Keycloak getInstance() {
-        return KeycloakBuilder
-                .builder()
-                .serverUrl(serverurl)
-                .realm(realm)
-                .username(adminusername)
-                .password(clientpassword)
-                .clientId(clientId)
-                .build();
-    }
-
-    public int createAccount(String email) {
-        CredentialRepresentation credential = new CredentialRepresentation();
-        credential.setType(CredentialRepresentation.PASSWORD);
-        credential.setValue(generatePassword(50));
-        credential.setTemporary(true);
-
-        UserRepresentation user = new UserRepresentation();
-        user.setUsername(email.toLowerCase(Locale.ROOT));
-        user.setCredentials(Arrays.asList(credential));
-
-        RealmResource ourRealm = getInstance().realm("master");
-        RolesResource roleList = ourRealm.roles();
-
-        UsersResource everyOne = ourRealm.users();
-
-        RoleRepresentation roleToUse = roleList.get("realmuser").toRepresentation();
-
-        javax.ws.rs.core.Response response = getInstance().realm(realm).users().create(user);
-
-        String userId = CreatedResponseUtil.getCreatedId(response);
-
-
-        UserResource oneUser = everyOne.get(userId);
-        oneUser.roles().realmLevel().add(Arrays.asList(roleToUse));
-        final int status = response.getStatus();
-
-        final String createdId = getCreatedId(response);
-
-        return HttpStatus.CREATED.value();
-    }
-
-    public static String getCreatedId(Response response) {
-        URI location = response.getLocation();
-        if (!response.getStatusInfo().equals(Response.Status.CREATED)) {
-            Response.StatusType statusInfo = response.getStatusInfo();
-            throw new RuntimeException("Create method returned status " +
-                    statusInfo.getReasonPhrase() + " (Code: " + statusInfo.getStatusCode() + "); expected status: Created (201)");
-        }
-        if (location == null) {
-            return null;
-        }
-        String path = location.getPath();
-        return path.substring(path.lastIndexOf('/') + 1);
-    }
-
-    public static String generatePassword(int numberOfDigits) {
-        String password = "";
-        for (int i = 0; i < numberOfDigits / 2; i++) {
-            password = password + (int) Math.floor(Math.random() * 9) + (char) Math.floor((Math.random() * 15) + 97);
-        }
-        return password;
-    }
-
     public AccessTokenResponse login(String email, String password) {
         return Keycloak.getInstance(
                 serverurl,
@@ -105,5 +30,10 @@ public class KeycloakClientService {
                 clientsecret)
                 .tokenManager()
                 .getAccessToken();
+    }
+
+    public void deleteUser(String username) {
+        Keycloak keycloak = Keycloak.getInstance(serverurl, realm, adminusername, clientpassword, clientId, clientsecret);
+        keycloak.realm(realm).users().search(username).get(0).setEnabled(false);
     }
 }
