3ce6a06b09aafb6adfcff5ecc25e6059d107ce90
Tibi <tothtibi30@gmail.com>
2021-02-16 11:41:47 +0100

    Added approve button to each element (row)

--- frontend/src/components/listofusers/ListElement.js
+++ frontend/src/components/listofusers/ListElement.js
@@ -4,12 +4,25 @@ import { useTranslation } from "react-i18next";
 const ListElement = ({ user }) => {
   const { t } = useTranslation();
 
+  const ApproveButton = (user) => {
+    return user.isApproved ? (
+      <button type="button" class="btn btn-success" disabled>
+        {t("approve")}
+      </button>
+    ) : (
+      <button type="button" class="btn btn-success">
+        {t("approve")}
+      </button>
+    );
+  };
+
   return (
     <tr>
       <td>{user.name}</td>
       <td>{user.email}</td>
       <td>{user.isApproved ? t("Yes") : t("No")}</td>
       <td>{user.dateOfRegistration}</td>
+      <td> {ApproveButton(user)} </td>
     </tr>
   );
 };
