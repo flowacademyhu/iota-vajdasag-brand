321eaaa4f1c52bdf9f469893018e0c11e77a2286
Tibi <tothtibi30@gmail.com>
2021-02-16 12:18:40 +0100

    Created mock axios endpoint for answering registering user call

--- frontend/src/communications/mockForUserApi.js
+++ frontend/src/communications/mockForUserApi.js
@@ -28,8 +28,12 @@ mock.onGet("/users").reply(200, {
     },
   ],
 });
-export const loginMock = () => {
-    const mock = new MockAdapter(axios)
-    mock.onPost("http://localhost:3000/api", { email: "teszt@teszt.com", password: "12345678" })
-        .reply(200, "84848fhgvripuerh98r4gu9hg4ru9hrv")
-}
+
+mock
+  .onPost("http://localhost:3000/api", {
+    email: "teszt@teszt.com",
+    password: "12345678",
+  })
+  .reply(200, "84848fhgvripuerh98r4gu9hg4ru9hrv");
+
+mock.onPut(`http://localhost:3000/users/2/approval`).reply(200);
