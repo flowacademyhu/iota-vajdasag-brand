f33a3b291f40b4d55edb86dcd4e2bfb997f1bec1
Dominik <gosidominik@gmail.com>
2021-02-16 14:46:26 +0100

    delete fixed to setEnabled to false when delete is called. config updated

--- backend/src/main/java/hu/flowacademy/vajdasagbrand/service/KeycloakClientService.java
+++ backend/src/main/java/hu/flowacademy/vajdasagbrand/service/KeycloakClientService.java
@@ -1,6 +1,7 @@
 package hu.flowacademy.vajdasagbrand.service;
 
 import org.keycloak.representations.AccessTokenResponse;
+import org.keycloak.representations.idm.UserRepresentation;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.keycloak.admin.client.Keycloak;
@@ -9,31 +10,53 @@ import org.keycloak.admin.client.Keycloak;
 public class KeycloakClientService {
 
     @Value("${keycloakBackendClient.serverUrl}")
-    private String serverurl;
+    private String keycloakBackendClientServerUrl;
     @Value("${keycloakBackendClient.realm}")
-    private String realm;
+    private String keycloakBackendClientRealm;
+    @Value("${keycloakBackendClient.realm2}")
+    private String keycloakBackendClientRealm2;
+    @Value("${keycloakBackendClient.realm_master}")
+    private String keycloakBackendClientRealmMaster;
     @Value("${keycloakBackendClient.adminusername}")
-    private String adminusername;
+    private String keycloakBackendClientRealmAdminUserName;
+    @Value("${keycloakBackendClient.adminpassword}")
+    private String keycloakBackendClientRealmAdminPassword;
     @Value("${keycloakBackendClient.clientpassword}")
-    private String clientpassword;
-    @Value("${keycloakBackendClient.clientId}")
-    private String clientId;
+    private String keycloakBackendClientRealmClientPassword;
+    @Value("${keycloakBackendClient.client-id}")
+    private String keycloakBackendClientRealmClientId;
+    @Value("${keycloakBackendClient.user-role}")
+    private String keycloakBackendClientUserRole;
+
+    @Value("${keycloak.realm}")
+    private String keycloakRealm;
+    @Value("${keycloak.auth-server-url}")
+    private String keycloakServerUrl;
+    @Value("${keycloak.resource}")
+    private String keycloakResource;
     @Value("${keycloak.credentials.secret}")
-    private String clientsecret;
+    private String keycloakCredentialsSecret;
+
 
     public AccessTokenResponse login(String email, String password) {
         return Keycloak.getInstance(
-                serverurl,
-                realm,
+                keycloakServerUrl,
+                keycloakRealm,
                 email, password,
-                clientId,
-                clientsecret)
+                keycloakResource,
+                keycloakCredentialsSecret)
                 .tokenManager()
                 .getAccessToken();
     }
 
     public void deleteUser(String username) {
-        Keycloak keycloak = Keycloak.getInstance(serverurl, realm, adminusername, clientpassword, clientId, clientsecret);
-        keycloak.realm(realm).users().search(username).get(0).setEnabled(false);
+        Keycloak keycloak = Keycloak.getInstance(keycloakBackendClientServerUrl,
+                keycloakBackendClientRealmMaster,
+                keycloakBackendClientRealmAdminUserName,
+                keycloakBackendClientRealmAdminPassword,
+                keycloakBackendClientRealmClientId);
+        UserRepresentation userdeleted = keycloak.realm(keycloakBackendClientRealm2).users().search(username).get(0);
+        userdeleted.setEnabled(false);
+        keycloak.realm(keycloakBackendClientRealm2).users().get(userdeleted.getId()).update(userdeleted);
     }
 }
