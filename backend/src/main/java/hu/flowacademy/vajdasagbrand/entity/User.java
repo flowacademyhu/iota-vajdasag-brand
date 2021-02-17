36ad9b0fba8df251486377223cfc64bd45d0b9c9
Dominik <gosidominik@gmail.com>
2021-02-17 08:36:09 +0100

    deletedAt added to user

---
+++ backend/src/main/java/hu/flowacademy/vajdasagbrand/entity/User.java
@@ -0,0 +1,36 @@
+package hu.flowacademy.vajdasagbrand.entity;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Table(name = "UserTable")
+public class User {
+
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    private String id;
+    private String fullName;
+    private String taxNumber;
+    private String address;
+    private String email;
+    private Type type;
+    private boolean enabled;
+    @JsonFormat(pattern = ("yyyy.MM.dd HH:mm:ss"))
+    private LocalDateTime registeredAt;
+    @JsonFormat(pattern = ("yyyy.MM.dd HH:mm:ss"))
+    private LocalDateTime deletedAt;
+}
