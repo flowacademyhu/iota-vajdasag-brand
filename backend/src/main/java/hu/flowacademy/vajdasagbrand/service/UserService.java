dbb6eb186e749ce439aeb9f876ebc8b5cc52ab78
Dominik <gosidominik@gmail.com>
2021-02-17 08:38:05 +0100

    validation to deleteById fixed

--- backend/src/main/java/hu/flowacademy/vajdasagbrand/service/UserService.java
+++ backend/src/main/java/hu/flowacademy/vajdasagbrand/service/UserService.java
@@ -1,26 +1,44 @@
 package hu.flowacademy.vajdasagbrand.service;
 
+import hu.flowacademy.vajdasagbrand.entity.Type;
+import lombok.AllArgsConstructor;
+import org.apache.commons.validator.routines.EmailValidator;
 import hu.flowacademy.vajdasagbrand.entity.User;
 import hu.flowacademy.vajdasagbrand.exception.ValidationException;
 import hu.flowacademy.vajdasagbrand.repository.UserRepository;
-import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.keycloak.admin.client.Keycloak;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import org.springframework.util.StringUtils;
+import javax.transaction.Transactional;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Slf4j
 @Service
-@Slf4j
-@RequiredArgsConstructor
+@AllArgsConstructor
 @Transactional
 public class UserService {
 
     private final UserRepository userRepository;
     private final KeycloakClientService keycloakClientService;
+
+    public User deleteById(String id) throws ValidationException {
+        Optional<User> user = userRepository.findById(id);
+        if (user.isEmpty()) {
+            throw new ValidationException("No user with given id: " + id);
+        }
+        if (!user.get().isEnabled()) {
+            throw new ValidationException("User already deleted");
+        }
+        User deleted = user.get();
+        if(!keycloakClientService.deleteUser(deleted.getEmail())) {
+            throw new ValidationException("No user with id in Keycloak");
+        }
+        deleted.setDeletedAt(LocalDateTime.now().withNano(0));
+        deleted.setEnabled(false);
+        userRepository.save(deleted);
+        return deleted;
+    }
 
     public User userRegistrationData(User user, String password) throws ValidationException {
         log.info("UserService called with: {}", user);
@@ -31,22 +49,6 @@ public class UserService {
         User result = userRepository.save(user);
         log.info("The result is : {}", result);
         return result;
-    }
-
-    public User deleteById(String id) throws ValidationException {
-       Optional<User> user = userRepository.findById(id);
-       if(user.isEmpty()) {
-           throw new ValidationException("No user with given id: " + id);
-       }
-       if(user.get().getDeletedAt() != null) {
-           throw new ValidationException("User already deleted");
-       }
-       User deleted = user.get();
-       keycloakClientService.deleteUser(deleted.getEmail());
-       deleted.setDeletedAt(LocalDateTime.now());
-       deleted.setEnabled(false);
-       userRepository.save(deleted);
-       return deleted;
     }
 
     private void validateUserData(User user) throws ValidationException {
@@ -68,4 +70,5 @@ public class UserService {
         if ((user.getTaxNumber()).isEmpty() && user.getType() == Type.COMPANY) {
             throw new ValidationException("Can not add tax number for individual members");
         }
+    }
 }
