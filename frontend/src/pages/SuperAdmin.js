1c49cd52eb78c0209bc953fbe6439d5c2773bc73
Tibi <tothtibi30@gmail.com>
2021-02-19 11:24:44 +0100

    Fixed bootstrap cols, moving them up a layer

--- frontend/src/pages/SuperAdmin.js
+++ frontend/src/pages/SuperAdmin.js
@@ -7,14 +7,14 @@ const SuperAdmin = () => {
   const { t } = useTranslation()
 
   return (
-    <div className="col-9">
+    <>
       <Route exact path="/super-admin">
         <h1>{t('welcome')}</h1>
       </Route>
       <Route path="/super-admin/users">
         <UsersList />
       </Route>
-    </div>
+    </>
   )
 }
 
