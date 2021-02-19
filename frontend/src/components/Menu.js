67eabc2086ee7fe6386ceecc5166d3961f080bb1
Tibi <tothtibi30@gmail.com>
2021-02-19 10:48:32 +0100

    Recreated MenuComponent, passing down icons as props

--- frontend/src/components/Menu.js
+++ frontend/src/components/Menu.js
@@ -1,38 +1,45 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next'
-import { NavLink } from 'react-router-dom'
+import MenuComponent from './MenuComponent'
 import { People, Newspaper, Shop, ArrowBarLeft } from 'react-bootstrap-icons'
 
 const Menu = () => {
   const { t } = useTranslation()
 
+  const menuItems = [
+    {
+      path: '/super-admin/users',
+      title: 'menu.users',
+      icon: People,
+    },
+    {
+      path: '/super-admin/products',
+      title: 'menu.products',
+      icon: Shop,
+    },
+    {
+      path: '/super-admin/events',
+      title: 'menu.events',
+      icon: Newspaper,
+    },
+    {
+      path: 'logout',
+      title: 'menu.signout',
+      icon: ArrowBarLeft,
+    },
+  ]
+
   return (
     <div className="border-right">
       <div className="list-group">
-        <NavLink className="nav-link" to="/super-admin/users">
-          <h4>
-            <People /> {t('menu.users')}
-          </h4>
-        </NavLink>
-        <NavLink className="nav-link" to="/super-admin/products">
-          <h4>
-            <Shop /> {t('menu.products')}
-          </h4>
-        </NavLink>
-        <NavLink className="nav-link" to="/super-admin/events">
-          <h4>
-            <Newspaper /> {t('menu.events')}
-          </h4>
-        </NavLink>
-        <NavLink
-          className="nav-link"
-          activeClassName="activeNavLink"
-          to="logout"
-        >
-          <h4>
-            <ArrowBarLeft /> {t('menu.signout')}
-          </h4>
-        </NavLink>
+        {menuItems.map((item, index) => (
+          <MenuComponent
+            key={index}
+            Icon={item.icon}
+            title={t(item.title)}
+            path={item.path}
+          />
+        ))}
       </div>
     </div>
   )
