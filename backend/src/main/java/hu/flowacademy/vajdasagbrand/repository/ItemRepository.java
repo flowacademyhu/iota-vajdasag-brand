43fe38c84ddae390fa5661ed9f72d057faf8b27f
Tibi <tothtibi30@gmail.com>
2021-02-22 20:37:50 +0100

    Added repository method findAll to get all Items

--- backend/src/main/java/hu/flowacademy/vajdasagbrand/repository/ItemRepository.java
+++ backend/src/main/java/hu/flowacademy/vajdasagbrand/repository/ItemRepository.java
@@ -4,10 +4,14 @@ import hu.flowacademy.vajdasagbrand.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ItemRepository extends JpaRepository<Item, String>{
 
     Optional<Item> findFirstByIdAndDeletedAtNull(String id);
+
+    @Override
+    List<Item> findAll();
 }
