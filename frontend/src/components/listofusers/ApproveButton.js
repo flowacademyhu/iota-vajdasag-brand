834af039551138fd919b1030e683a6663f0b5ae8
Tibi <tothtibi30@gmail.com>
2021-02-22 23:54:56 +0100

    Code style fix

--- frontend/src/components/listofusers/ApproveButton.js
+++ frontend/src/components/listofusers/ApproveButton.js
@@ -3,7 +3,7 @@ import { Button } from 'react-bootstrap'
 import useUsers from '../useUsers'
 import { useTranslation } from 'react-i18next'
 
-const ApproveButton = ({userId}) => {
+const ApproveButton = ({ userId }) => {
   const { sendRegistrationApproval } = useUsers()
   const { t } = useTranslation()
 
