ce0baa88472d715a75103dcbb05f40cd764bc9e4
Dominik <gosidominik@gmail.com>
2021-02-23 21:57:17 +0100

    methods updated to use dtos and some unnecessary lines removed

--- backend/src/main/java/hu/flowacademy/vajdasagbrand/service/UserService.java
+++ backend/src/main/java/hu/flowacademy/vajdasagbrand/service/UserService.java
@@ -1,42 +1,38 @@
 package hu.flowacademy.vajdasagbrand.service;
 
-import hu.flowacademy.vajdasagbrand.entity.Type;
+import hu.flowacademy.vajdasagbrand.configuration.persistence.sql.entity.Type;
+import hu.flowacademy.vajdasagbrand.dto.UserDTO;
 import hu.flowacademy.vajdasagbrand.exception.UserNotEnabledException;
+import hu.flowacademy.vajdasagbrand.repository.CommonUserRepository;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.validator.routines.EmailValidator;
-import hu.flowacademy.vajdasagbrand.entity.User;
 import hu.flowacademy.vajdasagbrand.exception.ValidationException;
-import hu.flowacademy.vajdasagbrand.repository.UserRepository;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
-
 import javax.transaction.Transactional;
 import java.time.LocalDateTime;
-import java.util.List;
 import java.util.Optional;
 
-@Slf4j
 @Service
 @RequiredArgsConstructor
 @Transactional
 public class UserService {
 
-    private final UserRepository userRepository;
+    private final CommonUserRepository userRepository;
     private final KeycloakClientService keycloakClientService;
 
-    public User deleteById(String id) throws ValidationException, UserNotEnabledException {
-        Optional<User> user = userRepository.findById(id);
+    public UserDTO deleteById(String id) throws ValidationException, UserNotEnabledException {
+        Optional<UserDTO> user = userRepository.findById(id);
         if (user.isEmpty()) {
             throw new ValidationException("No user with given id: " + id);
         }
         if (!user.get().isEnabled()) {
             throw new UserNotEnabledException("User is not enabled");
         }
-        User deleted = user.get();
+        UserDTO deleted = user.get();
         if (!keycloakClientService.deleteUser(deleted.getEmail())) {
             throw new ValidationException("No user with id in Keycloak");
         }
@@ -47,18 +43,15 @@ public class UserService {
         return deleted;
     }
 
-    public User userRegistrationData(User user, String password) throws ValidationException {
-        log.info("UserService called with: {}", user);
+    public UserDTO userRegistrationData(UserDTO user, String password) throws ValidationException {
         validateUserData(user);
 
         keycloakClientService.createAccount(user.getEmail(), password);
         user.setRegisteredAt(LocalDateTime.now());
-        User result = userRepository.save(user);
-        log.info("The result is : {}", result);
-        return result;
+        return userRepository.save(user);
     }
 
-    private void validateUserData(User user) throws ValidationException {
+    private void validateUserData(UserDTO user) throws ValidationException {
         if (!StringUtils.hasText(user.getFullName())) {
             throw new ValidationException("Didn't get full name");
         }
@@ -79,7 +72,7 @@ public class UserService {
         }
     }
 
-    public Page<User> getUsers(String orderBy, int pageNum, int limit) {
+    public Page<UserDTO> getUsers(String orderBy, int pageNum, int limit) {
         return userRepository.findAll(PageRequest.of(pageNum, limit, Sort.by(Sort.Direction.DESC, orderBy)));
     }
 }
