e571c43b113c7ded805b1a86fce6e8acc58bab53
Dominik <gosidominik@gmail.com>
2021-02-23 15:54:17 +0100

    missing annotation fixed

--- backend/src/main/java/hu/flowacademy/vajdasagbrand/repository/CommonItemRepository.java
+++ backend/src/main/java/hu/flowacademy/vajdasagbrand/repository/CommonItemRepository.java
@@ -1,11 +1,11 @@
 package hu.flowacademy.vajdasagbrand.repository;
 
-import hu.flowacademy.vajdasagbrand.configuration.persistence.sql.entity.Item;
 import hu.flowacademy.vajdasagbrand.dto.ItemDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
+@Repository
 public interface CommonItemRepository extends JpaRepository<ItemDTO, String> {
 
     Optional<ItemDTO> findFirstByIdAndDeletedAtNull(String id);
