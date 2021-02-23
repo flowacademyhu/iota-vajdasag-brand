a77c3a07d7d8198210b599966aa59e03c108e40f
Dominik <gosidominik@gmail.com>
2021-02-23 21:55:33 +0100

    save, findById and findAll declared

--- backend/src/main/java/hu/flowacademy/vajdasagbrand/repository/CommonUserRepository.java
+++ backend/src/main/java/hu/flowacademy/vajdasagbrand/repository/CommonUserRepository.java
@@ -1,9 +1,15 @@
 package hu.flowacademy.vajdasagbrand.repository;
 
-import hu.flowacademy.vajdasagbrand.dto.UserDTO;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public interface CommonUserRepository extends JpaRepository<UserDTO, String> {
+import hu.flowacademy.vajdasagbrand.dto.UserDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Optional;
+
+public interface CommonUserRepository {
+
+    UserDTO save(UserDTO userDTO);
+    Optional<UserDTO> findById(String id);
+    Page<UserDTO> findAll(Pageable pageable);
 }
