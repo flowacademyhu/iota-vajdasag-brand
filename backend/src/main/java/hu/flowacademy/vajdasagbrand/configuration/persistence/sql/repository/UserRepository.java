2557df1d187c09b817f69e4886b9e72b57ead8e9
Dominik <gosidominik@gmail.com>
2021-02-23 21:54:40 +0100

    UserRepository updated to use dto

--- backend/src/main/java/hu/flowacademy/vajdasagbrand/configuration/persistence/sql/repository/UserRepository.java
+++ backend/src/main/java/hu/flowacademy/vajdasagbrand/configuration/persistence/sql/repository/UserRepository.java
@@ -1,9 +1,9 @@
 package hu.flowacademy.vajdasagbrand.configuration.persistence.sql.repository;
 
-import hu.flowacademy.vajdasagbrand.configuration.persistence.sql.entity.User;
+import hu.flowacademy.vajdasagbrand.dto.UserDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, String>{
+public interface UserRepository extends JpaRepository<UserDTO, String>{
 }
