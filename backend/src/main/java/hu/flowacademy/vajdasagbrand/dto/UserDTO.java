d3ed31826228f44aac917750df7e7a8e3ae011de
Dominik <gosidominik@gmail.com>
2021-02-23 21:37:46 +0100

    Id, enabled, registeredAt and deletedAt added

--- backend/src/main/java/hu/flowacademy/vajdasagbrand/dto/UserDTO.java
+++ backend/src/main/java/hu/flowacademy/vajdasagbrand/dto/UserDTO.java
@@ -1,16 +1,27 @@
 package hu.flowacademy.vajdasagbrand.dto;
 
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import hu.flowacademy.vajdasagbrand.entity.Type;
+import hu.flowacademy.vajdasagbrand.configuration.persistence.sql.entity.Type;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserDTO {
+
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    private String id;
     @JsonProperty("full_name")
     private String fullName;
     @JsonProperty("tax_number")
@@ -19,4 +30,9 @@ public class UserDTO {
     private String email;
     private Type type;
     private String password;
+    private boolean enabled;
+    @JsonFormat(pattern = ("yyyy.MM.dd HH:mm:ss"))
+    private LocalDateTime registeredAt;
+    @JsonFormat(pattern = ("yyyy.MM.dd HH:mm:ss"))
+    private LocalDateTime deletedAt;
 }
