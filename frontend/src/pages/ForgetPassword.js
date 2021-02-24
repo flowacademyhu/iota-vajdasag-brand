1cee23b316cf57494fe94007b667c7dbd2297fce
zterhes <zterhes1@gmail.com>
2021-02-24 12:50:05 +0100

    adding forgetpassword page

--- frontend/src/pages/ForgetPassword.js
+++ frontend/src/pages/ForgetPassword.js
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useTranslation } from 'react-i18next'
+import { string } from "yup";
+import { Button } from 'react-bootstrap'
+
+
 
 const ForgetPassword = () => {
-    return ( 
+    const { t } = useTranslation()
+    const [email, setEmail] = useState("");
+    const [valid, setValid] = useState();
+
+    const handleSubmit = () => {
+        setValid(
+            string()
+                .email()
+                .isValidSync(email)
+        );
+        if (valid) {
+            console.log(email)
+        }
+    }
+
+    const handleChange = e => {
+        const value = e.currentTarget.value;
+        setEmail(value);
+    };
+
+    return (
         <>
+            <input type="text" value={email} onChange={handleChange} />
+            <h1>{valid ? "is email" : "is not email"}</h1>
+            <Button variant="primary" type="submit" onClick={handleSubmit}>
+                {t('login.buttontext')}
+            </Button>
         </>
-     );
+    );
 }
- 
+
 export default ForgetPassword;
