55e39329d319640e53cb77b2767c8e9d08e7bc01
pakaikarolina <pakaikarolina@gmail.com>
2021-02-24 14:48:02 +0100

    put @Builder

--- backend/src/main/java/hu/flowacademy/vajdasagbrand/dto/UserDTO.java
+++ backend/src/main/java/hu/flowacademy/vajdasagbrand/dto/UserDTO.java
@@ -4,12 +4,14 @@ package hu.flowacademy.vajdasagbrand.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import hu.flowacademy.vajdasagbrand.entity.Type;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class UserDTO {
     @JsonProperty("full_name")
     private String fullName;
