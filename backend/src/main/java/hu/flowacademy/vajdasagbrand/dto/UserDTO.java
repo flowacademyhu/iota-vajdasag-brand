a13519431077da1454bca87e2bc9d487b272169a
pakaikarolina <pakaikarolina@gmail.com>
2021-02-24 21:46:56 +0100

    changed fields

--- backend/src/main/java/hu/flowacademy/vajdasagbrand/dto/UserDTO.java
+++ backend/src/main/java/hu/flowacademy/vajdasagbrand/dto/UserDTO.java
@@ -1,18 +1,26 @@
 package hu.flowacademy.vajdasagbrand.dto;
 
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import hu.flowacademy.vajdasagbrand.entity.Type;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class UserDTO {
+
+    private String id;
     @JsonProperty("full_name")
     private String fullName;
     @JsonProperty("tax_number")
@@ -21,4 +29,9 @@ public class UserDTO {
     private String email;
     private Type type;
     private String password;
+    private boolean enabled;
+    @JsonFormat(pattern = ("yyyy.MM.dd HH:mm:ss"))
+    private LocalDateTime registeredAt;
+    @JsonFormat(pattern = ("yyyy.MM.dd HH:mm:ss"))
+    private LocalDateTime deletedAt;
 }
