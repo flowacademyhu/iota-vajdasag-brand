f076719544e826638ec0f38a28fdbda50142eef8
zterhes <zterhes1@gmail.com>
2021-02-24 10:00:16 +0100

    clear comments

--- frontend/src/components/listofproducts/DeleteProductButton.js
+++ frontend/src/components/listofproducts/DeleteProductButton.js
@@ -3,13 +3,11 @@ import { Button, Modal } from 'react-bootstrap'
 import { useTranslation } from 'react-i18next/'
 import DeletionResponseModal from '../listofusers/DeletionResponseModal'
 import { deleteProduct } from '../../communications/userApi'
-//import useProducts from '../useProducts'
 
 const DeleteModalButton = ({ productId }) => {
   const [showConfirmDeletion, setShowConfirmDeletion] = useState(false)
   const [showResponseModal, setShowResponseModal] = useState(false)
   const [responseModalTitle, setResponseModalTitle] = useState('')
-  //const { fetchProducts } = useProducts() fejlesztés alatt áll
   const { t } = useTranslation()
 
   const confirmModalHandler = (session) => {
