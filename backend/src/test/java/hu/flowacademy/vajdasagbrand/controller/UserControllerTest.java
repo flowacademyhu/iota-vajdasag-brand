a3fd6cfa926222120e4234eaf17ccfd9b6cf4be1
pakaikarolina <pakaikarolina@gmail.com>
2021-02-24 21:44:53 +0100

    created login tests for user and admin

--- backend/src/test/java/hu/flowacademy/vajdasagbrand/controller/UserControllerTest.java
+++ backend/src/test/java/hu/flowacademy/vajdasagbrand/controller/UserControllerTest.java
@@ -6,6 +6,7 @@ import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import lombok.extern.slf4j.Slf4j;
 import com.github.javafaker.Faker;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -13,6 +14,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import static hu.flowacademy.vajdasagbrand.helpers.UserHelper.login;
+import static hu.flowacademy.vajdasagbrand.helpers.UserHelper.loginWithSuperadmin;
 import static io.restassured.RestAssured.given;
 
 
@@ -25,6 +28,7 @@ class UserControllerTest {
     @LocalServerPort
     private int port;
     private static final Faker faker = new Faker();
+    private static UserDTO userDTO;
 
 
     @BeforeEach
@@ -32,32 +36,43 @@ class UserControllerTest {
         RestAssured.port = port;
     }
 
-    @Test
-    void loginwithCompanyAdmin() {
-
-    }
-
-    @Test
-    void deleteUser() {
+    @BeforeAll
+    private static void beforeAll() {
+        userDTO = UserDTO.builder()
+                .email(faker.internet().emailAddress())
+                .fullName(faker.chuckNorris().fact())
+                .password(faker.chuckNorris().fact())
+                .address(faker.address().fullAddress())
+                .taxNumber(faker.number().digit())
+                .type(Type.COMPANY)
+                .build();
     }
 
     @Test
     void userRegistration() {
         given().log().all()
-                .body(UserDTO.builder()
-                        .email(faker.internet().emailAddress())
-                        .fullName(faker.chuckNorris().fact())
-                        .password(faker.chuckNorris().fact())
-                        .address(faker.address().fullAddress())
-                        .taxNumber(faker.number().digit())
-                        .type(Type.COMPANY)
-                        .build())
+                .body(userDTO)
                 .contentType(ContentType.JSON)
                 .when().post("/api/registration")
                 .then()
                 .assertThat()
                 .statusCode(201);
     }
+
+    @Test
+    void loginwithCompanyAdmin() {
+        login(userDTO.getEmail(), userDTO.getPassword());
+    }
+
+    @Test
+    void loginwithSuperAdmin() {
+        loginWithSuperadmin();
+    }
+
+    @Test
+    void deleteUser() {
+    }
+
 
     @Test
     void approveRegistration() {
