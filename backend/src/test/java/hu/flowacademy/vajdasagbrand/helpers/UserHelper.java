55ebb64be74730a15ea728849382456106874d0b
pakaikarolina <pakaikarolina@gmail.com>
2021-02-24 21:43:37 +0100

    added userhelper

---
+++ backend/src/test/java/hu/flowacademy/vajdasagbrand/helpers/UserHelper.java
@@ -0,0 +1,38 @@
+package hu.flowacademy.vajdasagbrand.helpers;
+
+import hu.flowacademy.vajdasagbrand.configuration.persistence.Role;
+import hu.flowacademy.vajdasagbrand.dto.UserDTO;
+import io.restassured.http.ContentType;
+import io.restassured.http.Header;
+import org.keycloak.representations.AccessTokenResponse;
+
+import static io.restassured.RestAssured.given;
+
+public class UserHelper {
+
+    public static Header getAuthorization(String token) {
+        return new Header("Authorization", "Bearer " + token);
+    }
+
+    public static String login(String email, String password) {
+        var resp = given().log().all()
+                .body(getUser(email, password))
+                .contentType(ContentType.JSON)
+                .when().post("/api/login")
+                .then()
+                .assertThat()
+                .contentType(ContentType.JSON)
+                .statusCode(200)
+                .extract().body().as(AccessTokenResponse.class);
+        return resp.getToken();
+    }
+
+    public static String loginWithSuperadmin() {
+       return login("superamind@sa.com", "1234");
+    }
+
+
+    private static UserDTO getUser(String email, String password) {
+        return UserDTO.builder().email(email).password(password).build();
+    }
+}
