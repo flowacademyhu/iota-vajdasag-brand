ffc4ec8b2a627c5b202e623fbdd09ad758414f68
Tibi <tothtibi30@gmail.com>
2021-02-24 10:23:06 +0100

    Deleted TODO in SuperAdminItemDTO

--- backend/src/main/java/hu/flowacademy/vajdasagbrand/dto/SuperAdminItemDTO.java
+++ backend/src/main/java/hu/flowacademy/vajdasagbrand/dto/SuperAdminItemDTO.java
@@ -9,7 +9,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class SuperAdminItemDTO extends CegAdminItemDTO{
-    // TODO: create Owner property and add DB connection
     private String owner;
 
     public SuperAdminItemDTO(String address, String city, Category category, String owner) {
