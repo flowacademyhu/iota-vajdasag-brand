6d022f8d372ba527332778328eee91b33b22133a
Dominik <gosidominik@gmail.com>
2021-02-25 12:04:31 +0100

    findall created

--- backend/src/main/java/hu/flowacademy/vajdasagbrand/repository/CommonItemRepository.java
+++ backend/src/main/java/hu/flowacademy/vajdasagbrand/repository/CommonItemRepository.java
@@ -1,6 +1,10 @@
 package hu.flowacademy.vajdasagbrand.repository;
 
+import hu.flowacademy.vajdasagbrand.dto.CegAdminItemDTO;
 import hu.flowacademy.vajdasagbrand.dto.ItemDTO;
+import org.springframework.security.core.Authentication;
+
+import java.util.List;
 import java.util.Optional;
 
 
@@ -9,5 +13,6 @@ public interface CommonItemRepository {
     ItemDTO save(ItemDTO itemDTO);
     Optional<ItemDTO> findById(String id);
     Optional<ItemDTO> findFirstById(String id);
+    List<ItemDTO> findAll();
 
 }
--- backend/src/main/java/hu/flowacademy/vajdasagbrand/repository/ItemRepositoryImpl.java
+++ backend/src/main/java/hu/flowacademy/vajdasagbrand/repository/ItemRepositoryImpl.java
@@ -2,11 +2,15 @@ package hu.flowacademy.vajdasagbrand.repository;
 
 import hu.flowacademy.vajdasagbrand.configuration.persistence.entity.Item;
 import hu.flowacademy.vajdasagbrand.configuration.persistence.repository.ItemRepository;
+import hu.flowacademy.vajdasagbrand.dto.CegAdminItemDTO;
 import hu.flowacademy.vajdasagbrand.dto.ItemDTO;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Repository
 public class ItemRepositoryImpl implements CommonItemRepository{
@@ -30,5 +34,10 @@ public class ItemRepositoryImpl implements CommonItemRepository{
     @Override
     public Optional<ItemDTO> findFirstById(String id) {
         return itemRepository.findFirstByIdAndDeletedAtNull(id).map(Item::toDTO);
+    }
+
+    @Override
+    public List<ItemDTO> findAll() {
+        return itemRepository.findAll().stream().map(Item::toDTO).collect(Collectors.toList());
     }
 }
