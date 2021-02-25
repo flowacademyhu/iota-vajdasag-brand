c1bb955670d779b0847a825300fc83b8b8108fe1
Dominik <gosidominik@gmail.com>
2021-02-25 09:30:18 +0100

    repositories fixed

---
+++ backend/src/main/java/hu/flowacademy/vajdasagbrand/configuration/persistence/repository/ItemRepository.java
@@ -0,0 +1,13 @@
+package hu.flowacademy.vajdasagbrand.configuration.persistence.repository;
+
+import hu.flowacademy.vajdasagbrand.configuration.persistence.entity.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ItemRepository extends JpaRepository<Item, String>{
+
+    Optional<Item> findFirstByIdAndDeletedAtNull(String id);
+}
--- backend/src/main/java/hu/flowacademy/vajdasagbrand/configuration/persistence/sql/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package hu.flowacademy.vajdasagbrand.configuration.persistence.sql.repository;
-
-import hu.flowacademy.vajdasagbrand.dto.UserDTO;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<UserDTO, String>{
-}
