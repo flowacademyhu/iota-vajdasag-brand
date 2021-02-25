7419de11337bb36a9c82a2d8e5417f6c1f9eff6e
Tibi <tothtibi30@gmail.com>
2021-02-25 11:37:07 +0100

    Added new fields to CegAdminDTO and SuperAdminDTO to return every Item field

--- backend/src/main/java/hu/flowacademy/vajdasagbrand/dto/CegAdminItemDTO.java
+++ backend/src/main/java/hu/flowacademy/vajdasagbrand/dto/CegAdminItemDTO.java
@@ -1,16 +1,35 @@
 package hu.flowacademy.vajdasagbrand.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import hu.flowacademy.vajdasagbrand.entity.Category;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class CegAdminItemDTO {
+
     private String id;
+    private String name;
+    private int score;
+    @Lob
+    private String bio;
     private String address;
     private String city;
+    @Enumerated(EnumType.STRING)
     private Category category;
+    private String coordinateX;
+    private String coordinateY;
+    private String phone;
+    private String website;
+    private String facebook;
+    private String instagram;
+    @JsonFormat(pattern = ("yyyy.MM.dd HH:mm:ss"))
+    private LocalDateTime deletedAt;
 }
