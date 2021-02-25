9db20718b19bcf2ad959e779b31f15f8b32be1a0
Tibi <tothtibi30@gmail.com>
2021-02-25 13:18:00 +0100

    Removed unused imports and unnecessary annotations

--- backend/src/main/java/hu/flowacademy/vajdasagbrand/dto/CegAdminItemDTO.java
+++ backend/src/main/java/hu/flowacademy/vajdasagbrand/dto/CegAdminItemDTO.java
@@ -5,24 +5,19 @@ import hu.flowacademy.vajdasagbrand.entity.Category;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.GenericGenerator;
 
-import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class CegAdminItemDTO {
-
     private String id;
     private String name;
     private int score;
-    @Lob
     private String bio;
     private String address;
     private String city;
-    @Enumerated(EnumType.STRING)
     private Category category;
     private String coordinateX;
     private String coordinateY;
