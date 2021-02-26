3c91e5261e5ed1f68c954e77ec2cf4d37a1516c5
Hegedűs Barbara <hegedusbarby@gmail.com>
2021-02-26 20:17:51 +0100

    formatting code

--- backend/src/main/java/hu/flowacademy/vajdasagbrand/service/EventService.java
+++ backend/src/main/java/hu/flowacademy/vajdasagbrand/service/EventService.java
@@ -17,33 +17,32 @@ public class EventService {
 
     public EventDTO createEvent(EventDTO event) throws ValidationException {
         validateEventData(event);
-
         return eventRepository.save(event);
     }
 
-    private void validateEventData(EventDTO event) throws ValidationException{
-        if(!StringUtils.hasText(event.getName())) {
+    private void validateEventData(EventDTO event) throws ValidationException {
+        if (!StringUtils.hasText(event.getName())) {
             throw new ValidationException("Didn't get name");
         }
-        if(!StringUtils.hasText(event.getBio())) {
+        if (!StringUtils.hasText(event.getBio())) {
             throw new ValidationException("Didn't get bio");
         }
-        if(!StringUtils.hasText(event.getPlace())) {
+        if (!StringUtils.hasText(event.getPlace())) {
             throw new ValidationException("Didn't get place");
         }
-        if(event.getItemId() == null) {
+        if (event.getItemId() == null) {
             throw new ValidationException("Didn't get place");
         }
-        if(event.getEventstart() == null) {
+        if (event.getEventstart() == null) {
             throw new ValidationException("Didn't get time for event start");
         }
-        if(event.getEventend() == null) {
+        if (event.getEventend() == null) {
             throw new ValidationException("Didn't get time for event end");
         }
-        if(event.getEventstart().isAfter(event.getEventend())) {
+        if (event.getEventstart().isAfter(event.getEventend())) {
             throw new ValidationException("Program start is after the program end time");
         }
-        if(event.getEventstart().equals(event.getEventend())) {
+        if (event.getEventstart().equals(event.getEventend())) {
             throw new ValidationException("The event start is the same time with event end");
         }
     }
