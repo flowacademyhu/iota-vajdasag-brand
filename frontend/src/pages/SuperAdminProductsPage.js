49b056ef7ebc3a5dd0d143d8d7c5b1f71da15c05
zterhes <zterhes1@gmail.com>
2021-02-26 10:07:46 +0100

    add routing for testing

--- frontend/src/pages/SuperAdminProductsPage.js
+++ frontend/src/pages/SuperAdminProductsPage.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import DeleteProductButton from '../components/listofproducts/DeleteProductButton'
 import { useTranslation } from 'react-i18next'
-import AddProductButton from "../components/listofproducts/AddProductButton";
+import AddAndProductButton from "../components/listofproducts/AddProductButton";
 
 const SuperAdminProductsPage = () => {
   const { t } = useTranslation()
   return (
     <div>
-      <h1>Just for testing new product button</h1>
-      <AddProductButton title={t("product.newitem")}/>
       <h1>Just for testing delete button</h1>
       <DeleteProductButton />
+      <h1>Just for testing new product button</h1>
+      <AddAndProductButton title={t("product.newitem")} />
+      <h1>Just for testing new product button</h1>
     </div>
   )
 }
