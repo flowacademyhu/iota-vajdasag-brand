8b70dce38b12bf937428d51083874fdfd869c3ab
Hegedűs Barbara <hegedusbarby@gmail.com>
2021-02-26 23:33:26 +0100

    created return value in EventControllerTest

--- backend/src/test/java/hu/flowacademy/vajdasagbrand/controller/EventControllerTest.java
+++ backend/src/test/java/hu/flowacademy/vajdasagbrand/controller/EventControllerTest.java
@@ -13,10 +13,16 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import static hu.flowacademy.vajdasagbrand.helpers.UserHelper.getAuthorization;
 import static hu.flowacademy.vajdasagbrand.helpers.UserHelper.loginWithSuperadminWithToken;
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
+
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(
@@ -33,7 +39,7 @@ class EventControllerTest {
         RestAssured.port = port;
     }
 
-    private ItemDTO createItem () {
+    private ItemDTO createItem() {
         return given().log().all()
                 .header(getAuthorization(loginWithSuperadminWithToken()))
                 .contentType(ContentType.JSON)
@@ -63,25 +69,35 @@ class EventControllerTest {
 
     @Test
     void eventRegistration() {
-        var eventstart = LocalDateTime.now();
-        var eventend = eventstart.plusMinutes(15);
+        String date = "2020.03.15 18:00:00";
+        String dateend = "2020.03.15 20:00:00";
+        String name = "Event name";
+        String bio = "Something bio";
+        String place = "Event place";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm:ss");
+        LocalDateTime eventstart = LocalDateTime.parse(date, formatter);
+        LocalDateTime eventend = LocalDateTime.parse(dateend, formatter);
         var eventDTO = EventDTO.builder()
-                .name(faker.chuckNorris().fact())
-                .bio(faker.weather().description())
+                .name(name)
+                .bio(bio)
                 .eventstart(eventstart)
                 .eventend(eventend)
-                .place(faker.country().capital())
+                .place(place)
                 .itemId(createItem().getId())
                 .build();
         given().log().all()
-                .contentType(ContentType.JSON)
                 .header(getAuthorization(loginWithSuperadminWithToken()))
                 .body(eventDTO)
                 .contentType(ContentType.JSON)
                 .when().post("/api/events")
                 .then()
                 .assertThat()
+                .body("id", notNullValue())
+                .body("name", equalTo(name))
+                .body("bio", equalTo(bio))
+                .body("place", equalTo(place))
+                .body("eventstart", equalTo(date))
+                .body("eventend", equalTo(dateend))
                 .statusCode(201);
     }
-
 }
