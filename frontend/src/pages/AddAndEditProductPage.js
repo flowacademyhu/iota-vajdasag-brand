ac55fd87d8d1601b861c02936109f206da16f80e
zterhes <zterhes1@gmail.com>
2021-02-26 10:07:19 +0100

    add new page

--- frontend/src/pages/AddAndEditProductPage.js
+++ frontend/src/pages/AddAndEditProductPage.js
@@ -1,12 +1,12 @@
 import React, { useState } from 'react'
 import { useTranslation } from 'react-i18next'
-import { Formik, Form } from 'formik'
+import { Formik, Form} from 'formik'
 import { Button } from 'react-bootstrap'
 import { useHistory } from 'react-router-dom'
 import InputField from '../components/InputField'
 import SelectCategory from '../components/listofproducts/SelectCategory'
 import validationEdit from '../communications/validationEdit'
-import { updateProductData } from '../communications/userApi'
+import { updateProductData, createProduct } from '../communications/userApi'
 import ResponseModal from '../components/modals/ResponseModal'
 
 const updateOldFieldsInItem = (toBeUpdated, product) => {
@@ -16,23 +16,63 @@ const updateOldFieldsInItem = (toBeUpdated, product) => {
   return product
 }
 
-const EditProductPage = ({ product }) => {
+const AddAndEditProductPage = ({ product, type }) => {
   const [showResponseModal, setShowResponseModal] = useState(false)
   const [responseModalTitle, setResponseModalTitle] = useState('')
   const { t } = useTranslation()
   let history = useHistory()
 
   const handleSubmit = async (value) => {
-    try {
-      await updateProductData(updateOldFieldsInItem(value))
-      setResponseModalTitle(t('editProduct.successfulEdition'))
-      setShowResponseModal(true)
-    } catch (error) {
-      setResponseModalTitle(t('editProduct.unsuccessfulEdition'))
-      setShowResponseModal(true)
-      history.push('/super-admin/products')
+    if (type === "update") {
+      try {
+        await updateProductData(updateOldFieldsInItem(value))
+        setResponseModalTitle(t('editProduct.successfulEdition'))
+        setShowResponseModal(true)
+      } catch (error) {
+        setResponseModalTitle(t('editProduct.unsuccessfulEdition'))
+        setShowResponseModal(true)
+        history.push('/super-admin/products')
+      }
+    } else {
+      try {
+        await createProduct(value)
+      } catch (error) {
+        console.log(error)
+      }
     }
   }
+
+  const handleFormInit = (type) => {
+    if (type === "update") {
+      return {
+        address: '',
+        city: '',
+        category: type === "update" ? product.category : '', // product.category,
+        coordinateX: '',
+        coordinateY: '',
+        phone: '',
+        website: '',
+        facebook: '',
+        instagram: '',
+      }
+    } else {
+      return {
+        name: '',
+        address: '',
+        city: '',
+        category: type === "update" ? product.category : '', // product.category,
+        bio: '',
+        coordinateX: '',
+        coordinateY: '',
+        score: '',
+        phone: '',
+        website: '',
+        facebook: '',
+        instagram: '',
+      }
+    }
+  }
+
 
   return (
     <div className="m-5">
@@ -40,7 +80,7 @@ const EditProductPage = ({ product }) => {
         initialValues={{
           address: '',
           city: '',
-          category: product.category, // product.category,
+          category: type === "update" ? product.category : '', // product.category,
           coordinateX: '',
           coordinateY: '',
           phone: '',
@@ -53,7 +93,18 @@ const EditProductPage = ({ product }) => {
       >
         <Form>
           <div className="d-flex flex-column justify-content-center align-content-center mx-auto">
-            <h3 className="text-center">{t('editProduct.title')}</h3>
+            <h3 className="text-center">{type === "update" ? t('editProduct.title') : t('editProduct.newAddingTitle')}</h3>
+            {type === "create" &&
+              <div className="my-2">
+                <InputField
+                  label={t('editProduct.name')}
+                  name="name"
+                  id="name"
+                  placeholder={t('editProduct.name')}
+                  type="text"
+                />
+              </div>
+            }
             <div className="my-2">
               <InputField
                 label={t('editProduct.address')}
@@ -75,6 +126,17 @@ const EditProductPage = ({ product }) => {
             <div className="my-2">
               <SelectCategory />
             </div>
+            {type === "create" &&
+              <div className="my-2">
+                <InputField
+                  label={t('editProduct.bio')}
+                  name="bio"
+                  id="bio"
+                  placeholder={t('editProduct.name')}
+                  type="textfield"
+                />
+              </div>
+            }
             <div className="my-2">
               <InputField
                 label={t('editProduct.coordinateX')}
@@ -93,6 +155,12 @@ const EditProductPage = ({ product }) => {
                 type="text"
               />
             </div>
+            {type === "create" &&
+              <div className="my-2">
+                <label for="score">{t('editProduct.score')}</label>
+                <input name="score" id="score" type="range"></input>
+              </div>
+            }
             <div className="my-2">
               <InputField
                 label={t('editProduct.phone')}
@@ -130,7 +198,7 @@ const EditProductPage = ({ product }) => {
               />
             </div>
             <Button variant="primary" type="submit" size="lg">
-              {t('editProduct.save')}
+              {type === "update" ? t('editProduct.save') : t('editProduct.create')}
             </Button>
           </div>
         </Form>
@@ -144,4 +212,4 @@ const EditProductPage = ({ product }) => {
   )
 }
 
-export default EditProductPage
+export default AddAndEditProductPage
