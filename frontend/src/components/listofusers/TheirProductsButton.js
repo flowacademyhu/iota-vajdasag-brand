38364014bdcf2a8ca60dfa5497bfd5e50de19af8
fhrmt <feher.mate.944@gmail.com>
2021-02-28 23:55:15 +0100

    simplified operator fixed in TheirProductsButton

--- frontend/src/components/listofusers/TheirProductsButton.js
+++ frontend/src/components/listofusers/TheirProductsButton.js
@@ -3,16 +3,18 @@ import { useTranslation } from 'react-i18next'
 import { Button } from 'react-bootstrap'
 import { NavLink } from 'react-router-dom'
 
-const TheirProductsButton = (user) => {
+const TheirProductsButton = ({ user }) => {
   const { t } = useTranslation()
   return (
-    user.isApproved && (
-      <NavLink to={`/super-admin/products/${user.id}/`}>
-        <Button type="button" variant="success">
-          {t('userListElement.products')}
-        </Button>
-      </NavLink>
-    )
+    <div>
+      {user.isApproved && (
+        <NavLink to={`/super-admin/products/${user.id}/`}>
+          <Button type="button" variant="success">
+            {t('userListElement.products')}
+          </Button>
+        </NavLink>
+      )}
+    </div>
   )
 }
 
