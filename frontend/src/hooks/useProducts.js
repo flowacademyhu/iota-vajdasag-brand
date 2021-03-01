d529c4b81285222f1a9809e7c259113ae4323fec
Tibi <tothtibi30@gmail.com>
2021-03-01 22:38:30 +0100

    Added search logic to useProducts, a similar approach was used in users list

--- frontend/src/hooks/useProducts.js
+++ frontend/src/hooks/useProducts.js
@@ -1,8 +1,19 @@
 import { useState, useEffect } from 'react'
 import { fetchProducts } from '../communications/userApi'
 
-const useProducts = () => {
+/*
+ * Removes all accents from words and makes them uppercase.
+ **/
+const makeWordComparable = (keyword) => {
+  return keyword
+    ?.normalize('NFD')
+    .replace(/[\u0300-\u036f]/g, '')
+    .toUpperCase()
+}
+
+const useProducts = (searchKeyword) => {
   const [listOfAllProducts, setListOfAllProducts] = useState([])
+  const [products, setProducts] = useState([])
 
   const getAllProducts = async () => {
     const fetchedProducts = await fetchProducts()
@@ -13,7 +24,21 @@ const useProducts = () => {
     getAllProducts()
   }, [])
 
-  return listOfAllProducts
+  useEffect(() => {
+    const searchWord = makeWordComparable(searchKeyword)
+    setProducts(
+      listOfAllProducts.filter(
+        (product) =>
+          makeWordComparable(product.name).includes(searchWord) ||
+          makeWordComparable(product.address).includes(searchWord) ||
+          makeWordComparable(product.city).includes(searchWord) ||
+          makeWordComparable(product.category).includes(searchWord) ||
+          makeWordComparable(product.owner).includes(searchWord)
+      )
+    )
+  }, [searchKeyword, listOfAllProducts])
+
+  return { products }
 }
 
 export default useProducts
