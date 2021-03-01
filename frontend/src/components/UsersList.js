8ea88a846f7663d33c7ded9698796a5cbe44fb01
Tibi <tothtibi30@gmail.com>
2021-03-01 21:00:44 +0100

    Moved Searchbar into "components" folder

--- frontend/src/components/UsersList.js
+++ frontend/src/components/UsersList.js
@@ -3,7 +3,7 @@ import { useTranslation } from 'react-i18next'
 import useUsers from './useUsers'
 import ResponseModal from '../components/modals/ResponseModal'
 import ListElement from './listofusers/ListElement'
-import Searchbar from './listofusers/Searchbar'
+import Searchbar from './Searchbar'
 import ListHeader from './listofusers/ListHeader'
 
 const UsersList = () => {
