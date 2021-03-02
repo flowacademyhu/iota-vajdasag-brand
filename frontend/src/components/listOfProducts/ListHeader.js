65d4d615bf562e4ecd94a2153f419f88bef88eff
Tibi <tothtibi30@gmail.com>
2021-03-02 12:38:38 +0100

    Added sorting sings to column headers

--- frontend/src/components/listOfProducts/ListHeader.js
+++ frontend/src/components/listOfProducts/ListHeader.js
@@ -1,17 +1,39 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next'
+import { SortDown, SortUpAlt } from 'react-bootstrap-icons'
 
 const ListHeader = () => {
   const { t } = useTranslation()
 
+  const SortingSign = (value) => {
+    if (sortKey === value) {
+      return isSortAscending ? <SortUpAlt /> : <SortDown />
+    }
+  }
+
   return (
     <thead>
       <tr>
-        <th scope="col">{t('product.name')}</th>
-        <th scope="col">{t('product.address')}</th>
-        <th scope="col">{t('product.city')}</th>
-        <th scope="col">{t('product.category')}</th>
-        <th scope="col">{t('product.owner')}</th>
+        <th scope="col" onClick={() => onColumnClick('name')}>
+          {t('product.name')}
+          {SortingSign('name')}
+        </th>
+        <th scope="col" onClick={() => onColumnClick('address')}>
+          {t('product.address')}
+          {SortingSign('address')}
+        </th>
+        <th scope="col" onClick={() => onColumnClick('city')}>
+          {t('product.city')}
+          {SortingSign('city')}
+        </th>
+        <th scope="col" onClick={() => onColumnClick('category')}>
+          {t('product.category')}
+          {SortingSign('category')}
+        </th>
+        <th scope="col" onClick={() => onColumnClick('ownerName')}>
+          {t('product.owner')}
+          {SortingSign('ownerName')}
+        </th>
         <th />
       </tr>
     </thead>
