413adcdabd9e930e8ad43f269c7353be3b605e65
Tibi <tothtibi30@gmail.com>
2021-03-02 12:00:29 +0100

    Added ownerName to SuperAdminDTO, so we dont have to make a new API call on FE for every list item

--- backend/src/main/java/hu/flowacademy/vajdasagbrand/dto/SuperAdminItemDTO.java
+++ backend/src/main/java/hu/flowacademy/vajdasagbrand/dto/SuperAdminItemDTO.java
@@ -13,10 +13,10 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 public class SuperAdminItemDTO extends CegAdminItemDTO {
-    private String owner;
+    private String ownerName;
 
-    public SuperAdminItemDTO(String id, String name, String score, String bio, String address, String contact, String city, String email, Category category, Subcategory subcategory, String coordinateX, String coordinateY, String phone, String website, String facebook, String instagram, LocalDateTime deletedAt, String owner) {
-        super(id, name, score, bio, address, contact, city, email, category, subcategory, coordinateX, coordinateY, phone, website, facebook, instagram, deletedAt);
-        this.owner = owner;
+    public SuperAdminItemDTO(String id, String name, String score, String bio, String address, String contact, String city, String email, Category category, Subcategory subcategory, String coordinateX, String coordinateY, String phone, String website, String facebook, String instagram, LocalDateTime deletedAt, String ownerId, String ownerName) {
+        super(id, name, score, bio, address, contact, city, email, category, subcategory, coordinateX, coordinateY, phone, website, facebook, instagram, deletedAt, ownerId);
+        this.ownerName = ownerName;
     }
 }
