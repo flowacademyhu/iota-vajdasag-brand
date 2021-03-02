bdd38d56e64d61ac59bd7cf35e448ab0785aa906
Tibi <tothtibi30@gmail.com>
2021-03-02 13:34:30 +0100

    I forgot to run code-style-fix

--- frontend/src/components/frequentlyUsedFunctions.js
+++ frontend/src/components/frequentlyUsedFunctions.js
@@ -2,7 +2,10 @@
  * Removes all accents from words and makes them uppercase.
  **/
 const removeAccentsFromWords = (word) => {
-  return word.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toUpperCase()
+  return word
+    .normalize('NFD')
+    .replace(/[\u0300-\u036f]/g, '')
+    .toUpperCase()
 }
 
 export { removeAccentsFromWords }
