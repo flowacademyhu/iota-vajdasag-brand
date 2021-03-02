9cf0a66930b0505a60cdcc321b225462aad8732a
Fehér Máté <feher.mate.944@gmail.com>
2021-03-02 09:23:46 +0100

    button navigates to the users productlist by userid

---
+++ frontend/src/components/listofusers/TheUsersProductsButton.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { useTranslation } from 'react-i18next'
+import { Button } from 'react-bootstrap'
+import { NavLink } from 'react-router-dom'
+
+const TheUsersProductsButton = ({ user }) => {
+  const { t } = useTranslation()
+
+  return (
+    <div>
+      {user.isApproved && (
+        <NavLink to={`/super-admin/items/${user.id}/`}>
+          <Button type="button" variant="success">
+            {t('userListElement.products')}
+          </Button>
+        </NavLink>
+      )}
+    </div>
+  )
+}
+
+export default TheUsersProductsButton
