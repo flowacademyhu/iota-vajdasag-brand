167884ba8f4564e6e48cf27cd9c072d8a6699d8a
Fehér Máté <feher.mate.944@gmail.com>
2021-03-02 11:52:44 +0100

    SingleCompanyProductList added

--- frontend/src/components/listofusers/TheUsersProductsButton.js
+++ frontend/src/components/listofusers/TheUsersProductsButton.js
@@ -3,15 +3,15 @@ import { useTranslation } from 'react-i18next'
 import { Button } from 'react-bootstrap'
 import { NavLink } from 'react-router-dom'
 
-const TheUsersProductsButton = (user) => {
+const TheUsersProductsButton = ({ user }) => {
   const { t } = useTranslation()
 
   return (
-    <NavLink to={`/super-admin/items/${user.id}/`}>
-      <Button type="button" variant="success">
-        {t('userListElement.products')}
-      </Button>
-    </NavLink>
+        <NavLink to={`/super-admin/items/${user.id}/`}>
+          <Button type="button" variant="success">
+            {t('userListElement.products')}
+          </Button>
+        </NavLink>
   )
 }
 
