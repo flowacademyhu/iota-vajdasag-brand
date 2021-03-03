a3b0bc593bd6a42f4bd8e9219704e602a7de2fc2
zterhes <zterhes1@gmail.com>
2021-03-03 14:00:44 +0100

    update menu to handling roles

--- frontend/src/components/Menu.js
+++ frontend/src/components/Menu.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import MenuComponent from './MenuComponent'
 import { People, Newspaper, Shop, ArrowBarLeft } from 'react-bootstrap-icons'
+import useLoggedInUser from "../hooks/useLoggedInUser";
 
-const menuItems = [
+const menuItemsSuperAdmin = [
   {
     path: '/super-admin/users',
     title: 'menu.users',
@@ -26,13 +27,41 @@ const menuItems = [
   },
 ]
 
+const menuItemsCompanyAdmin = [
+  {
+    path: '/company-admin/items',
+    title: 'menu.items',
+    icon: Shop,
+  },
+  {
+    path: '/company-admin/events',
+    title: 'menu.events',
+    icon: Newspaper,
+  },
+  {
+    path: 'logout',
+    title: 'menu.signout',
+    icon: ArrowBarLeft,
+  },
+]
+
 const Menu = () => {
+  const userRole = useLoggedInUser().role
+  console.log(userRole)
   const { t } = useTranslation()
 
   return (
     <div className="border-right">
       <div className="list-group">
-        {menuItems.map((item, index) => (
+        {userRole === "CegAdmin" && menuItemsCompanyAdmin.map((item, index) => (
+          <MenuComponent
+            key={index}
+            Icon={item.icon}
+            title={t(item.title)}
+            path={item.path}
+          />
+        ))}
+        {userRole === "SuperAdmin" && menuItemsSuperAdmin.map((item, index) => (
           <MenuComponent
             key={index}
             Icon={item.icon}
