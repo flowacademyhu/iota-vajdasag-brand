50e1299cfbbaa4e646a89b139bb1d054ff06d46b
Dominik <gosidominik@gmail.com>
2021-03-03 11:20:17 +0100

    jpaRepositories removed

--- backend/src/main/java/hu/flowacademy/vajdasagbrand/persistence/repository/EventJPARepository.java
+++
@@ -1,9 +0,0 @@
-package hu.flowacademy.vajdasagbrand.persistence.repository;
-
-import hu.flowacademy.vajdasagbrand.persistence.entity.Event;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EventJPARepository extends JpaRepository<Event, String> {
-}
