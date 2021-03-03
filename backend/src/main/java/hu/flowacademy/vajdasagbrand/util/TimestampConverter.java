15aa2662a7303c87ba03761d85a986fbe91de4b2
Dominik <gosidominik@gmail.com>
2021-03-03 19:45:31 +0100

    refactor to handle null

--- backend/src/main/java/hu/flowacademy/vajdasagbrand/util/TimestampConverter.java
+++ backend/src/main/java/hu/flowacademy/vajdasagbrand/util/TimestampConverter.java
@@ -5,13 +5,16 @@ import lombok.NonNull;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.Date;
+import java.util.Optional;
 
 public final class TimestampConverter {
     public static Timestamp toTimestamp(@NonNull LocalDateTime localDateTime) {
-        return Timestamp.of(Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant()));
+        return Optional.ofNullable(localDateTime)
+                .map(ldt -> Timestamp.of(Date.from(ldt.atZone(ZoneId.systemDefault()).toInstant()))).orElse(null);
     }
 
     public static LocalDateTime toLocalDateTime(@NonNull Timestamp timestamp) {
-        return timestamp.toDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+        return Optional.ofNullable(timestamp)
+                .map(ts -> ts.toDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime()).orElse(null);
     }
 }
