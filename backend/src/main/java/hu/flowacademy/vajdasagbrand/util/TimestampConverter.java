c82666f30f4eb5ebf8bfeeed8abdc62a4d2ce480
Dominik <gosidominik@gmail.com>
2021-03-03 16:11:19 +0100

    timestampconverter added

--- backend/src/main/java/hu/flowacademy/vajdasagbrand/util/TimestampConverter.java
+++ backend/src/main/java/hu/flowacademy/vajdasagbrand/util/TimestampConverter.java
@@ -1,2 +1,17 @@
-package hu.flowacademy.vajdasagbrand.util;public class TimestampConverter {
+package hu.flowacademy.vajdasagbrand.util;
+
+import com.google.cloud.Timestamp;
+import lombok.NonNull;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+
+public final class TimestampConverter {
+    public static Timestamp toTimestamp(@NonNull LocalDateTime localDateTime) {
+        return Timestamp.of(Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant()));
+    }
+
+    public static LocalDateTime toLocalDateTime(@NonNull Timestamp timestamp) {
+        return timestamp.toDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+    }
 }
