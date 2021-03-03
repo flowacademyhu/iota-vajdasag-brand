658b1174742d2f3db60b58d31f691c55c58e105e
zterhes <zterhes1@gmail.com>
2021-03-03 16:58:23 +0100

    add product events button

---
+++ frontend/src/components/listofevents/EventsProductButton.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Button, Modal } from 'react-bootstrap'
+
+
+const EventsProductButton = () => {
+  const { t } = useTranslation()
+
+    return ( 
+        <>
+        <Link>
+        <Button>
+
+        </Button>
+        </Link>
+        </>
+     );
+}
+ 
+export default EventsProductButton;
