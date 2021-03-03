63647665049be9730be954d37e42a4f653fb95f2
Dominik <gosidominik@gmail.com>
2021-03-03 20:23:45 +0100

    removed annotation

--- backend/src/main/java/hu/flowacademy/vajdasagbrand/util/TimestampConverter.java
+++ backend/src/main/java/hu/flowacademy/vajdasagbrand/util/TimestampConverter.java
@@ -8,12 +8,12 @@ import java.util.Date;
 import java.util.Optional;
 
 public final class TimestampConverter {
-    public static Timestamp toTimestamp(@NonNull LocalDateTime localDateTime) {
+    public static Timestamp toTimestamp(LocalDateTime localDateTime) {
         return Optional.ofNullable(localDateTime)
                 .map(ldt -> Timestamp.of(Date.from(ldt.atZone(ZoneId.systemDefault()).toInstant()))).orElse(null);
     }
 
-    public static LocalDateTime toLocalDateTime(@NonNull Timestamp timestamp) {
+    public static LocalDateTime toLocalDateTime(Timestamp timestamp) {
         return Optional.ofNullable(timestamp)
                 .map(ts -> ts.toDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime()).orElse(null);
     }
