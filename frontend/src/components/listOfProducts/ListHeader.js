2389667e324afbd159033eb59c1b2e1591fe47ac
Tibi <tothtibi30@gmail.com>
2021-03-03 13:52:39 +0100

    Moved SortingSign component outside ListHeader and passed it down as props

--- frontend/src/components/listOfProducts/ListHeader.js
+++ frontend/src/components/listOfProducts/ListHeader.js
@@ -1,15 +1,8 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next'
-import { SortDown, SortUpAlt } from 'react-bootstrap-icons'
 
-const ListHeader = ({ onColumnClick, isSortAscending, sortKey }) => {
+const ListHeader = ({ onColumnClick, SortingSign }) => {
   const { t } = useTranslation()
-
-  const SortingSign = (value) => {
-    if (sortKey === value) {
-      return isSortAscending ? <SortUpAlt /> : <SortDown />
-    }
-  }
 
   return (
     <thead>
