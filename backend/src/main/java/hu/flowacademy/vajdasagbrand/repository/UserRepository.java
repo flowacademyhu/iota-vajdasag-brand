ac4a8736e769c8097cfb73db47710865e9ac65ef
Dominik <gosidominik@gmail.com>
2021-03-03 15:12:38 +0100

    findall fixed

--- backend/src/main/java/hu/flowacademy/vajdasagbrand/repository/UserRepository.java
+++ backend/src/main/java/hu/flowacademy/vajdasagbrand/repository/UserRepository.java
@@ -1,8 +1,7 @@
 package hu.flowacademy.vajdasagbrand.repository;
 
 import com.google.api.core.ApiFuture;
-import com.google.cloud.firestore.Firestore;
-import com.google.cloud.firestore.QuerySnapshot;
+import com.google.cloud.firestore.*;
 import hu.flowacademy.vajdasagbrand.dto.UserDTO;
 import hu.flowacademy.vajdasagbrand.persistence.entity.User;
 import lombok.RequiredArgsConstructor;
@@ -13,6 +12,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
 import java.util.Comparator;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
@@ -43,21 +43,21 @@ public class UserRepository {
         }
     }
 
-    public Page<UserDTO> findAllUsers(Pageable pageable) {
-        ApiFuture<QuerySnapshot> documents = firestore.collection(COLLECTION).get();
-        try {
-          return new PageImpl<>(documents.get().getDocuments().stream()
-                    .map(queryDocumentSnapshot -> queryDocumentSnapshot.toObject(User.class))
-                    .skip(pageable.getOffset())
-                    .limit(pageable.getPageSize())
-                    .sorted(Comparator.comparing(User::getRegisteredAt))
-                    .map(User::toDTO)
-                    .collect(Collectors.toList()));
-        } catch (InterruptedException | ExecutionException e) {
-            log.error("Exception occurred while retrieving all document", e);
-            return Page.empty();
-        }
-    }
+//    public Page<UserDTO> findAllUsers(Pageable pageable) {
+//        ApiFuture<QuerySnapshot> documents = firestore.collection(COLLECTION).get();
+//        try {
+//          return new PageImpl<>(documents.get().getDocuments().stream()
+//                    .map(queryDocumentSnapshot -> queryDocumentSnapshot.toObject(User.class))
+//                    .skip(pageable.getOffset())
+//                    .limit(pageable.getPageSize())
+//                    .sorted(Comparator.comparing(User::getRegisteredAt))
+//                    .map(User::toDTO)
+//                    .collect(Collectors.toList()));
+//        } catch (InterruptedException | ExecutionException e) {
+//            log.error("Exception occurred while retrieving all document", e);
+//            return Page.empty();
+//        }
+//    }
 
     public Optional<UserDTO> findByEmail(String email) {
         ApiFuture<QuerySnapshot> documents = firestore.collection(COLLECTION).get();
@@ -70,6 +70,23 @@ public class UserRepository {
         } catch (InterruptedException | ExecutionException e) {
             log.error("No user with given email", e);
             return Optional.empty();
+        }
+    }
+
+    public Page<UserDTO> findAllUsers(Pageable pageable) {
+        CollectionReference collection = firestore.collection(COLLECTION);
+        try {
+            pageable.getSort().stream()
+                    .forEach(order -> collection.orderBy(order.getProperty(),
+                            order.isAscending() ? Query.Direction.ASCENDING : Query.Direction.DESCENDING));
+
+            return new PageImpl<>(collection.get().get().getDocuments().stream()
+                    .map(queryDocumentSnapshot -> queryDocumentSnapshot.toObject(User.class))
+                    .map(User::toDTO)
+                    .collect(Collectors.toList()));
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+            return Page.empty();
         }
     }
 }
