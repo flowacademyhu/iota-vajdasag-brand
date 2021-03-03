d49d80faeebca1d7303a73e169ac69db660f0f0c
Dominik <gosidominik@gmail.com>
2021-03-03 11:17:52 +0100

    eventrepository changed to match firebase

--- backend/src/main/java/hu/flowacademy/vajdasagbrand/repository/EventRepository.java
+++ backend/src/main/java/hu/flowacademy/vajdasagbrand/repository/EventRepository.java
@@ -1,11 +1,9 @@
 package hu.flowacademy.vajdasagbrand.repository;
 
-import com.google.cloud.firestore.CollectionReference;
 import com.google.cloud.firestore.DocumentReference;
 import com.google.cloud.firestore.Firestore;
 import hu.flowacademy.vajdasagbrand.dto.EventDTO;
 import hu.flowacademy.vajdasagbrand.persistence.entity.Event;
-import hu.flowacademy.vajdasagbrand.persistence.repository.EventJPARepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -14,17 +12,18 @@ import java.util.UUID;
 @Repository
 @RequiredArgsConstructor
 public class EventRepository {
-
-    public static final String PLACE = "places";
+    
     public static final String EVENTS = "events";
+    public static final String CITIES = "cities";
+    public static final String LANGUAGES = "languages";
     private final Firestore firestore;
 
     public EventDTO save(EventDTO eventDTO) {
-        DocumentReference events = firestore.collection(EVENTS).document(eventDTO.getName().toLowerCase());
-        DocumentReference places = events.collection(PLACE).document(eventDTO.getPlace().toLowerCase());
+        DocumentReference places = firestore.collection(CITIES).document(eventDTO.getPlace().toLowerCase());
+        DocumentReference lang = places.collection(LANGUAGES).document("hu");
         Event event = Event.fromDTO(eventDTO);
         event.setId(UUID.randomUUID().toString());
-        places.collection(EVENTS).document(event.getId()).set(event);
+        lang.collection(EVENTS).document(event.getId()).set(event);
         return event.toDTO();
     }
 }
