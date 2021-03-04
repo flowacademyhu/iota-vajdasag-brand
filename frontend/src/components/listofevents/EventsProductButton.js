53be2e85b312711de2e9d055960faf23b694fb13
zterhes <zterhes1@gmail.com>
2021-03-04 22:43:54 +0100

    work on events button

--- frontend/src/components/listofevents/EventsProductButton.js
+++ frontend/src/components/listofevents/EventsProductButton.js
@@ -1,19 +1,28 @@
 import React from 'react';
 import { Button, Modal } from 'react-bootstrap'
+import { useTranslation } from 'react-i18next'
+import { Link } from 'react-router-dom'
+
+
 
 
 const EventsProductButton = () => {
   const { t } = useTranslation()
 
-    return ( 
-        <>
-        <Link>
+  return (
+    <>
+      <Link to={{
+        pathname: "/super-admin/events",
+      search: "?sort=name",
+      hash: "#the-hash",
+      state: { fromDashboard: true }}}
+      >
         <Button>
-
+          {t("eventList.button")}
         </Button>
-        </Link>
-        </>
-     );
+      </Link>
+    </>
+  );
 }
- 
+
 export default EventsProductButton;
