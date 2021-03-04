1bb6c72e56d53cd7c99faedf042fb71a378fcc64
Tibi <tothtibi30@gmail.com>
2021-03-04 13:03:47 +0100

    Created initial add event page

--- frontend/src/pages/AddEventPage.js
+++ frontend/src/pages/AddEventPage.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Formik, Form } from 'formik'
 import { useTranslation } from 'react-i18next'
 import { Button } from 'react-bootstrap'
+import InputField from '../components/InputField'
 import eventAddValidation from '../validations/eventAddValidation'
 
 const AddEventPage = () => {
@@ -23,9 +24,18 @@ const AddEventPage = () => {
     >
       <Form>
         <div className="d-flex flex-column justify-content-center align-content-center mx-auto">
-          <h3 className="text-center">{t('editProduct.title')}</h3>
+          <h3 className="text-center">{t('addEvent.title')}</h3>
+          <div className="my-2">
+            <InputField label={t('addEvent.name')} name="name" type="text" />
+          </div>
+          <div className="my-2">
+            <InputField label={t('addEvent.bio')} name="bio" type="text" />
+          </div>
+          <div className="my-2">
+            <InputField label={t('addEvent.place')} name="place" type="text" />
+          </div>
           <Button variant="primary" type="submit" size="lg">
-            {t('editProduct.save')}
+            {t('addEvent.save')}
           </Button>
         </div>
       </Form>
