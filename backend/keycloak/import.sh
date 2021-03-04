4fdcc6d12d5cf9595eca31add41bd0c68d33a01a
Roland Németh <info@nemethroland.hu>
2021-03-04 17:29:02 +0100

    Improve Keycloak on dev machine

--- backend/keycloak/import.sh
+++ backend/keycloak/import.sh
@@ -3,25 +3,26 @@
 set -euo pipefail
 
 TIMEOUT_SECONDS=300
-LOGFILE=/tmp/keycloak-export.log
+LOGFILE=/tmp/keycloak-import.log
 JSON_EXPORT_FILE=/keycloak/realm.json
 
 rm -f ${LOGFILE}
 
-echo "Export started, please wait..."
+echo "Import started, please wait..."
 
 timeout ${TIMEOUT_SECONDS}s \
     /opt/jboss/tools/docker-entrypoint.sh \
-        -Dkeycloak.migration.action=export \
+        -Dkeycloak.migration.action=import \
         -Dkeycloak.migration.provider=singleFile \
         -Dkeycloak.migration.file=${JSON_EXPORT_FILE} \
+        -Dkeycloak.profile.feature.upload_scripts=enabled \
         -Djboss.socket.binding.port-offset=99 \
     > ${LOGFILE} &
 
 PID="${!}"
 
 timeout ${TIMEOUT_SECONDS}s \
-    grep -m 1 "Export finished successfully" <(tail -f ${LOGFILE})
+    grep -m 1 "Import finished successfully" <(tail -f ${LOGFILE})
 
 kill ${PID}
 
