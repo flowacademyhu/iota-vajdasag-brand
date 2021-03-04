71b61cbf6d22ca0aa1d12cb8fc08b9931a804d30
Dominik <gosidominik@gmail.com>
2021-03-04 12:50:18 +0100

    springboootapp updated

--- backend/src/main/java/hu/flowacademy/vajdasagbrand/VajdasagBrandApplication.java
+++ backend/src/main/java/hu/flowacademy/vajdasagbrand/VajdasagBrandApplication.java
@@ -2,8 +2,9 @@ package hu.flowacademy.vajdasagbrand;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = JpaRepositoriesAutoConfiguration.class)
 public class VajdasagBrandApplication {
 
 	public static void main(String[] args) {
