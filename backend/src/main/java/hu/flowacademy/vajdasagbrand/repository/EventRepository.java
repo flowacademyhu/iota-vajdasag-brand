47d5aeda9680e5f17742f4e83ec8c12bd24a0a91
Dominik <gosidominik@gmail.com>
2021-03-04 01:16:03 +0100

    findAll updeted for firebase

--- backend/src/main/java/hu/flowacademy/vajdasagbrand/repository/EventRepository.java
+++ backend/src/main/java/hu/flowacademy/vajdasagbrand/repository/EventRepository.java
@@ -1,17 +1,24 @@
 package hu.flowacademy.vajdasagbrand.repository;
 
-import com.google.cloud.firestore.DocumentReference;
-import com.google.cloud.firestore.Firestore;
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.*;
 import hu.flowacademy.vajdasagbrand.dto.EventDTO;
 import hu.flowacademy.vajdasagbrand.persistence.entity.Event;
+import hu.flowacademy.vajdasagbrand.persistence.entity.User;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
+import java.util.concurrent.ExecutionException;
+import java.util.stream.Collectors;
 
+@Slf4j
 @Repository
 @RequiredArgsConstructor
 public class EventRepository {
@@ -33,6 +40,22 @@ public class EventRepository {
     }
 
     public Page<EventDTO> findAllEvents(Pageable pageable) {
-        return eventRepository.findAll(pageable).map(Event::toDTO);
+        try {
+            Query collection = firestore.collectionGroup(EVENTS);
+
+            var query = pageable.getSort().stream().findFirst()
+                    .map(order -> collection.orderBy(order.getProperty(),
+                            order.isAscending() ? Query.Direction.ASCENDING : Query.Direction.DESCENDING))
+                    .orElse(collection);
+
+            return new PageImpl(query.limit(pageable.getPageSize()).offset((int) pageable.getOffset())
+                    .get().get().getDocuments().stream()
+                    .map(queryDocumentSnapshot -> queryDocumentSnapshot.toObject(Event.class))
+                    .map(Event::toDTO)
+                    .collect(Collectors.toList()));
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+            return Page.empty();
+        }
     }
 }
